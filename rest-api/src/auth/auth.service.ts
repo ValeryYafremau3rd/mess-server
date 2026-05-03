d55e57accbb6ff31f31cfa7da856454400c89be5
Valery <valeryyafremau@gmail.com>
2026-05-03 10:23:45 +0400

    api gateway

--- rest-api/src/auth/auth.service.ts
+++ rest-api/src/auth/auth.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
   async generateToken(userId: number) {
-    const payload = { sub: userId };
+    const payload = { sub: userId, iss: this.configService.get('ISSUER_ID') };
     return {
       access_token: this.jwtService.sign(payload),
     };
